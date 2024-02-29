2405bc7bfc84207e91b807d983c5a34eea28e98d
TwistRU <pavlyuk.rs@dvfu.ru>
2024-02-29 15:36:46 +1000

    Added more info columns to Talon model

--- admission/peopleQueue/models.py
+++ admission/peopleQueue/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Talon(models.Model):
     name = models.CharField(max_length=5)
-    createdAt = models.DateTimeField(auto_now_add=True)
+    compliting_by = models.OneToOneField(User, related_name="compliting_talon", default=None, on_delete=models.SET_NULL, null=True)
+    completed = models.BooleanField(default=False)
+    completed_by = models.ForeignKey(User, related_name="completed_talons", default=None, on_delete=models.SET_NULL, null=True)
+    completed_at = models.DateTimeField(default=None, null=True)
+    updated_by = models.ForeignKey(User, related_name="talons_updated", on_delete=models.SET_NULL, null=True)
+    updated_at= models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name="talons_created", on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
