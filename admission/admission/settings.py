4f13cbfdb6fab7b476e9906054269029ed211234
TwistRU <pavlyuk.rs@dvfu.ru>
2024-02-29 15:36:07 +1000

    daphne asgi server added

--- admission/admission/settings.py
+++ admission/admission/settings.py
@@ -30,12 +30,13 @@ SECRET_KEY = "django-insecure-bcjt4+(g=$9pztiufa9w9!+&_2&vz-j5m-acs)q#bp(#st$_wf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -100,27 +101,27 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    # },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    # },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    # },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    # },
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Vladivostok"
 
 USE_I18N = True
 
@@ -157,3 +158,10 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
  }
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': [
+#         'rest_framework.renderers.JSONRenderer',
+#         'rest_framework.renderers.BrowsableAPIRenderer',
+#     ]
+# }
--- admission/peopleQueue/routing.py
+++ admission/peopleQueue/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from .consumers import MyConsumer
+from .consumers import AsyncChatConsumer
 
 websocket_urlpatterns = [
-   re_path(r'ws/some_path/$', MyConsumer.as_asgi()),
+   re_path(r'ws/chat/$', AsyncChatConsumer.as_asgi()),
 ]
