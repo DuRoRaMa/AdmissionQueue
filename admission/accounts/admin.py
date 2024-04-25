8bce2090871f226ae6498489950e85b4efa42e20
TwistRU <pavlyuk.rs@dvfu.ru>
2024-04-25 13:51:55 +1000

    CustomUser done

--- admission/accounts/admin.py
+++ admission/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import CustomUser
 
 # Register your models here.
-admin.register(User)
+admin.site.register(CustomUser, UserAdmin)
--- admission/accounts/models.py
+++ admission/accounts/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class User(AbstractUser):
-    pass
+from peopleQueue.models import Talon
+
+
+class CustomUser(AbstractUser):
+    def get_started_talons(self):
+        return self.talon_logs.filter(action__name="Started").values("talon")
+
+    def get_completed_talons(self):
+        return self.talon_logs.filter(action__name="Completed").values("talon")
+
+    def get_current_talon(self) -> Talon | None:
+        """Если существует запись в таблице логов о старте талона и не существует о конце, то передается талон, если же запись о конце существует, то возвращается None
+
+        Returns:
+            Talon | None
+        """
+        if self.talon_logs.filter(action__name="Started").exists():
+            last_talon = self.talon_logs.filter(
+                action__name="Started").last().talon
+            if last_talon.is_over():
+                return None
+            else:
+                return last_talon
+        else:
+            return None
