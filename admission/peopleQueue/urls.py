5d281d6fd240ed8625606f1b49b1cfddd5bf145f
TwistRU <pavlyuk.rs@dvfu.ru>
2024-05-13 17:44:11 +1000

    fixed some urls

--- admission/peopleQueue/urls.py
+++ admission/peopleQueue/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 app_name = 'queue'
 urlpatterns = [
-    re_path(r"^operator/", views.OperatorAPIView.as_view(), name="OperatorAPI"),
-    re_path(r"^talon/", views.TalonListCreateAPIView.as_view(), name="TalonAPI"),
-    re_path(r"^location/", views.OperatorLocationListAPIView.as_view()),
-    re_path(r"^settings/",
-            views.OperatorSettingsAPIView.as_view()),
+    re_path(r"^operator/talon/action",
+            views.OperatorTalonActionAPIView.as_view()),
+    re_path(r"^talon/", views.TalonListCreateAPIView.as_view()),
+    re_path(r"^locations/", views.OperatorLocationListAPIView.as_view()),
+    re_path(r"^operator/settings", views.OperatorSettingsAPIView.as_view()),
     re_path(r"^purposes/", views.TalonPurposesListAPIView.as_view()),
-    re_path(r"tablo/", views.TabloAPIView().as_view())
+    re_path(r"^tablo/", views.TabloAPIView().as_view())
 ]
