251c23e3134079f0e18cf80b140d495e39b8d132
TwistRU <pavlyuk.rs@dvfu.ru>
2024-06-04 17:05:08 +1000

    modified signals for tg_chat notifications

--- admission/peopleQueue/signals.py
+++ admission/peopleQueue/signals.py
@@ -1,11 +1,14 @@
+from datetime import timedelta
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from asgiref.sync import sync_to_async
 from channels.layers import get_channel_layer
+from django.contrib.auth import get_user_model
+import django_rq
+from rq import Queue
+from .tasks import send_to_admin, send_to_tg_chat
 
-from .models import TalonLog, Talon, OperatorQueue
+from .models import OperatorLocation, TalonLog, Talon, OperatorQueue
 from .schema import schema
-from .serializers import TalonLogSerializer
 
 channel_layer = get_channel_layer()
 
@@ -38,12 +41,42 @@ async def on_talon_post_save(sender, instance, created, raw, using, update_field
         if OQ:
             user = OQ.user
             user.aassign_talon()
+        send_to_admin.delay('Talon created', instance)
 
 
 @receiver(post_save, sender=TalonLog)
-async def on_talonlog_post_save(sender, instance, created, raw, using, update_fields, **kwargs):
+async def on_talonlog_post_save(sender, instance: TalonLog, created: bool, raw, using, update_fields, **kwargs):
     result = await schema.execute_async(talonlog_query, variables={'talonLog': instance}, context={'talonLog': instance})
     await channel_layer.group_send('tablo', {
         "type": 'talonLog.create',
         'message': result.data
     })
+    talon = instance.talon
+    if talon.tg_chat_id:
+        minutes = 0
+        text = ""
+        if instance.action == TalonLog.Actions.ASSIGNED:
+            # await instance.arefresh_from_db(fields=['created_by'])
+            created_by = await get_user_model().objects.select_related(
+                'operator_settings'
+            ).aget(
+                pk=instance.created_by.pk
+            )
+            operator_settings = created_by.operator_settings
+            location = await OperatorLocation.objects.aget(
+                settings=operator_settings.pk
+            )
+            text = f"""
+            Статус талона {talon.name} обновлен!\nОператор ожидает вас за столом: {location.name}"""
+        elif instance.action == TalonLog.Actions.CANCELLED:
+            text = f"""
+            Статус талона {talon.name} обновлен!\nК сожалению Ваш талон больше недействителен.\nЧтобы взять новый талон обратитесь, пожалуйста, на стойку ресепшена."""
+        elif instance.action == TalonLog.Actions.COMPLETED:
+            minutes = 5
+            text = f"""
+            Статус талона {talon.name} обновлен!\nСпасибо, что обратились в Приемную комиссию ДВФУ.\nСвой отзыв Вы можете оставить в меню "Мои талоны"""
+        queue: Queue = django_rq.queues.get_queue()
+        queue.enqueue_in(timedelta(minutes=minutes),
+                         send_to_tg_chat,
+                         talon.tg_chat_id, text)
+        # send_to_tg_chat.delay(talon.tg_chat_id, text)
