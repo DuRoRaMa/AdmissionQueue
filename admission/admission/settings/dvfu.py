78a026dc36a848a04943573f9f8d1ee4df12824d
TwistRU <pavlyuk.rs@dvfu.ru>
2024-06-18 16:30:42 +1000

    dvfu prod

---
+++ admission/admission/settings/dvfu.py
@@ -0,0 +1,71 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'aq.dvfu.ru',
+    '127.0.0.1',
+    'localhost'
+]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOWED_ORIGINS = [
+    'https://aq.dvfu.ru',
+    'http://127.0.0.1:8000',
+]
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = [
+    'https://aq.dvfu.ru',
+]
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = "6379"
+REDIS_URL = (REDIS_HOST, REDIS_PORT)
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [REDIS_URL],
+        },
+    },
+}
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': REDIS_HOST,
+        'PORT': REDIS_PORT,
+        'DEFAULT_TIMEOUT': 360,
+    },
+}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": 5432,
+    }
+}
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
