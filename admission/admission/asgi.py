bfd311b3b3730cb3fc92b1d5a2629764c6518a5b
Vinogradov Vladislav <150411162+DuRoRaMa@users.noreply.github.com>
2025-06-16 14:06:20 +1000

    Update asgi.py

--- admission/admission/asgi.py
+++ admission/admission/asgi.py
@@ -1,14 +1,5 @@
-"""
-ASGI config for admission project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-if True:
-    import django
-    django.setup()
+import django
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -17,12 +8,11 @@ from django.urls import re_path
 from strawberry.channels import GraphQLWSConsumer
 from peopleQueue.schema import schema
 
-
 django_asgi_app = get_asgi_application()
 gql_ws_consumer = GraphQLWSConsumer.as_asgi(schema=schema)
 
 websocket_urlpatterns = [
-    re_path(r"graphql/", gql_ws_consumer),
+    re_path(r"graphql/?$", gql_ws_consumer),  # Обрабатывает /graphql и /graphql/
 ]
 
 application = ProtocolTypeRouter(
