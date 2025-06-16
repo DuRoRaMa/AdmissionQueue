40a1f2e0c47167ecd68f36621b0b2cadc413e103
Vinogradov Vladislav <150411162+DuRoRaMa@users.noreply.github.com>
2025-06-16 15:33:35 +1000

    Update dvfu.py

--- admission/admission/settings/dvfu.py
+++ admission/admission/settings/dvfu.py
@@ -17,6 +17,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = [
     'https://vinogradov-it.ru',
     'https://www.vinogradov-it.ru',
+    'wss://vinogradov-it.ru'
 ]
 
 # Важно для работы за прокси (Nginx)
@@ -25,7 +26,8 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # Доверенные источники для CSRF
 CSRF_TRUSTED_ORIGINS = [
     'https://vinogradov-it.ru',
-    'https://www.vinogradov-it.ru'
+    'https://www.vinogradov-it.ru',
+    'wss://vinogradov-it.ru'
 ]
 
 # Безопасные куки (включить для HTTPS)
