976695027ae97ee993990fee32cb6368a1cd3d09
Vinogradov Vladislav <150411162+DuRoRaMa@users.noreply.github.com>
2025-06-16 15:02:33 +1000

    Update asgi.py

--- admission/admission/asgi.py
+++ admission/admission/asgi.py
@@ -1,63 +1,33 @@
-import os
-import django
-from django.core.asgi import get_asgi_application
+"""
+ASGI config for admission project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+if True:
+    import django
+    django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from strawberry.channels import GraphQLWSConsumer
 from peopleQueue.schema import schema
-from cryptography.fernet import Fernet, InvalidToken
-import logging
 
-logger = logging.getLogger(__name__)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admission.settings')
-django.setup()
+django_asgi_app = get_asgi_application()
+gql_ws_consumer = GraphQLWSConsumer.as_asgi(schema=schema)
 
-class AuthenticatedGraphQLWSConsumer(GraphQLWSConsumer):
-    async def connect(self):
-        try:
-            # Получаем токен из query string или cookies
-            query_params = self.scope.get('query_string', b'').decode()
-            token = None
-            
-            if 'token=' in query_params:
-                token = query_params.split('token=')[1].split('&')[0]
-            elif 'headers' in self.scope:
-                headers = dict(self.scope['headers'])
-                if b'cookie' in headers:
-                    cookies = headers[b'cookie'].decode().split(';')
-                    for cookie in cookies:
-                        if 'auth_token=' in cookie.strip():
-                            token = cookie.strip().split('auth_token=')[1]
-                            break
-            
-            if not token:
-                logger.warning("No token provided in WebSocket connection")
-                await self.close(code=4403)  # Unauthorized
-                return
+websocket_urlpatterns = [
+    re_path(r"graphql/", gql_ws_consumer),
+]
 
-            # Валидация токена
-            Fernet(os.environ['FERNET_KEY']).decrypt(token.encode())
-            
-            await super().connect()
-            logger.info(f"WebSocket connection established for {self.scope['user']}")
-
-        except InvalidToken:
-            logger.error("Invalid token in WebSocket connection")
-            await self.close(code=4401)  # Unauthorized
-        except Exception as e:
-            logger.error(f"WebSocket connection error: {str(e)}")
-            await self.close(code=4400)  # Internal Server Error
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter([
-                re_path(r"^graphql/?$", AuthenticatedGraphQLWSConsumer.as_asgi(schema=schema)),
-            ])
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
