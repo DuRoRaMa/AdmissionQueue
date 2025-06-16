a4bd218fdcb67399dcfc522cfe662e15d6c51720
Vinogradov Vladislav <150411162+DuRoRaMa@users.noreply.github.com>
2025-06-16 15:04:07 +1000

    Update urls.py

--- admission/admission/urls.py
+++ admission/admission/urls.py
@@ -1,3 +1,19 @@
+"""
+URL configuration for admission project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, re_path, path
@@ -5,20 +21,15 @@ from django.views.decorators.csrf import csrf_exempt
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from strawberry.django.views import AsyncGraphQLView
 from peopleQueue.schema import schema
-from .middleware import WebSocketAuthMiddleware
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
-    
-    # HTTP GraphQL endpoint
     re_path(r'^graphql', csrf_exempt(AsyncGraphQLView.as_view(
         graphiql=settings.DEBUG,
         schema=schema,
         subscriptions_enabled=True
     ))),
-    
-    # API endpoints
     re_path(r'api/', include([
         re_path(r'^v1/', include([
             re_path(r'^auth/', include('djoser.urls')),
