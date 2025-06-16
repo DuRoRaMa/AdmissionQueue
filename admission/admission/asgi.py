e452bb983b124c770fb5e4d37c38d36d680e34ec
Vinogradov Vladislav <150411162+DuRoRaMa@users.noreply.github.com>
2025-06-16 14:38:58 +1000

    Update asgi.py

--- admission/admission/asgi.py
+++ admission/admission/asgi.py
@@ -5,14 +5,16 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
-from strawberry.channels import GraphQLWSConsumer
+from strawberry.django.views import AsyncGraphQLView
 from peopleQueue.schema import schema
 
 django_asgi_app = get_asgi_application()
-gql_ws_consumer = GraphQLWSConsumer.as_asgi(schema=schema)
 
 websocket_urlpatterns = [
-    re_path(r"graphql/?$", gql_ws_consumer),  # Обрабатывает /graphql и /graphql/
+    re_path(r"graphql/?$", AsyncGraphQLView.as_asgi(
+        schema=schema,
+        subscriptions_enabled=True
+    )),
 ]
 
 application = ProtocolTypeRouter(
