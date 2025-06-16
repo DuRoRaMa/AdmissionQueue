59d484d0cad25b63f64438aa697da7a0d097ce30
Vinogradov Vladislav <150411162+DuRoRaMa@users.noreply.github.com>
2025-06-16 15:36:11 +1000

    Update asgi.py

--- admission/admission/asgi.py
+++ admission/admission/asgi.py
@@ -1,33 +1,41 @@
 """
 ASGI config for admission project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-if True:
-    import django
-    django.setup()
 
+import os
+import django
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import re_path
 from strawberry.channels import GraphQLWSConsumer
 from peopleQueue.schema import schema
 
+# Инициализация Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admission.settings')
+django.setup()
 
-django_asgi_app = get_asgi_application()
-gql_ws_consumer = GraphQLWSConsumer.as_asgi(schema=schema)
+# WebSocket consumer
+class CustomGraphQLWSConsumer(GraphQLWSConsumer):
+    async def connect(self):
+        # Дополнительная проверка аутентификации
+        if not self.scope.get("user", None):
+            await self.close(code=4401)  # Unauthorized
+            return
+        await super().connect()
+
+gql_ws_consumer = CustomGraphQLWSConsumer.as_asgi(schema=schema)
 
 websocket_urlpatterns = [
-    re_path(r"graphql/", gql_ws_consumer),
+    re_path(r"^graphql/$", gql_ws_consumer),
 ]
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
+})
